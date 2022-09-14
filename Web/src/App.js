848c3df9baaf0ffab344ee85563b70f1c6f21d36
Michael R Ryan <45245378+MichaelRRyan@users.noreply.github.com>
2022-09-14 10:34:50 +0100

    Added necessary packages and basic routing

--- Web/src/App.js
+++ Web/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+const Navigation = () => (
+  <div>
+    <Link to="/home">Home</Link>
+    <Outlet />
+  </div>
+);
+
+const Home = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </header>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        {/* <Route path="shop" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} /> */}
+      </Route>
+    </Routes>
   );
 }
 
