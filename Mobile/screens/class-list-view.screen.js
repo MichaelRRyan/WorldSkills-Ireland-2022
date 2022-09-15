bcfd40fe95c2820befdc773bc86daab185ad88cf
Michael R Ryan <45245378+MichaelRRyan@users.noreply.github.com>
2022-09-15 16:02:06 +0100

    Added the student list

--- Mobile/screens/class-list-view.screen.js
+++ Mobile/screens/class-list-view.screen.js
@@ -1,8 +1,20 @@
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { useContext } from "react";
+import { View, Text, Button, StyleSheet, FlatList } from "react-native";
+import { ExamContext } from "../contexts/exam.context";
 
 const ClassListViewScreen = ({ navigation }) => {
+  const { currentExam } = useContext(ExamContext);
+
   return (
     <View style={styles.container}>
+      <FlatList
+        data={currentExam.students}
+        renderItem={({ item }) => (
+          <Text>
+            {item.name} {item.mark}
+          </Text>
+        )}
+      />
       <Button
         title="Conclude Exam"
         color="rgb(255, 139, 0)"
